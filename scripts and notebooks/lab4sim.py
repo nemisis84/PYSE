25b3b6c7cae30530ad662a59bb8392e135ef739b
simenmyr <simenmyr@stud.ntnu.no>
2022-11-18 17:04:35 +0100

    Final delivery

--- scripts and notebooks/lab4sim.py
+++ scripts and notebooks/lab4sim.py
@@ -2,6 +2,7 @@ import simpy
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import math
 
 
 class Custumer:
@@ -11,17 +12,19 @@ class Custumer:
         self.A = 1
 
     def checkout_process(self, checkout_lam):
+        global queue_times, counters_working, waiting_time_and_counters
         time_stamp1 = self.env.now
         req = self.checkout.request()
         yield req
         time_stamp2 = self.env.now
         Tq = time_stamp2-time_stamp1
+        if not counters_working in waiting_time_and_counters:
+            waiting_time_and_counters[counters_working] = []
+        waiting_time_and_counters[counters_working] += [Tq]
         checkout_time = neg_exp(checkout_lam)
         yield self.env.timeout(checkout_time)
         self.checkout.release(req)
-        global queue_times, counters_working, counters_working_when_custumer_arrives
         queue_times.append(Tq)
-        counters_working_when_custumer_arrives.append(counters_working)
 
 
 class Counter:
@@ -33,7 +36,7 @@ class Counter:
     def simulate_failure(self, repair_time_lam):
         timestamp = False
 
-        req = self.checkout.PriorityRequest(priority=0)
+        req = self.checkout.request(priority=0)
         yield req
 
         global counters_working
@@ -80,7 +83,7 @@ def generate_failure(env, counter, repair_time_lam, failure_interval_lam):
             yield env.timeout(time_to_next_failure)
             env.process(counter.simulate_failure(repair_time_lam))
         else:
-            yield env.timeout(0.1)
+            yield env.timeout(0.001)
 
 
 def availability(up_time, down_time):
@@ -98,7 +101,7 @@ def simulate():
     # Simpy setup and resources
     env = simpy.Environment()
     repairman = simpy.Resource(env, capacity=1)
-    checkout = simpy.Resource(env, capacity=4)
+    checkout = simpy.PriorityResource(env, capacity=4)
     counter = Counter(env, checkout, repairman)
 
     # Start processes
@@ -113,14 +116,41 @@ def simulate():
     A_list.append(availability(up_time, down_time))
 
 
+def calculate_queue_time(d: list):
+
+    mean = []
+    std = []
+    for key in d.keys():
+        if bool(d[key]):
+            mean.append(np.array(d[key]).mean())
+            std.append(np.array(d[key]).std())
+
+    return mean, std
+
+
+def C_formula(A, n):
+    upper = A**(n)*n/(math.factorial(n)*(n-A))
+    lower = 0
+    for i in range(n):
+        lower += A**(i)/math.factorial(i)
+    lower += A**(n)*n/(math.factorial(n)*(n-A))
+    return upper/lower
+
+
+def expected_waiting_time(A, mu, n):
+    return C_formula(A, n)/(mu*(n-A))
+
+
 if __name__ == "__main__":
 
     queue_times_list = []
     down_time_list = []
-    counters_working_when_custumer_arrives_list = []
+    waiting_time_and_counters = {}
+
     A_list = []
-    for i in range(365*3):
+    for i in range(10*365):
         print(f"Run{i}")
+
         queue_times = []
         down_time = 0
         counters_working = 4
@@ -128,11 +158,32 @@ if __name__ == "__main__":
         simulate()
         queue_times_list.append(np.array(queue_times))
         down_time_list.append(down_time)
-        counters_working_when_custumer_arrives_list.append(
-            np.array(counters_working_when_custumer_arrives))
 
     queue_times_list = np.hstack(np.array(queue_times_list))
-    counters_working_when_custumer_arrives_list = np.hstack(np.array(
-        counters_working_when_custumer_arrives_list))
     A_list = np.hstack(np.array(A_list))
-    print(f'Results:\nAvailability A = {A_list.mean()}\nA std = {A_list.std()}\nMean queue time T_q = {queue_times_list.mean()}\nQueue time std={queue_times_list.std()}\nMean counters available when in front of line = {counters_working_when_custumer_arrives_list.mean()}\Counters available when in front of line std = {counters_working_when_custumer_arrives_list.std()}')
+    mean, std = calculate_queue_time(waiting_time_and_counters)
+    mean.reverse(), std.reverse()
+    print(f"Mean waiting time simulation: {mean} std: {std}")
+    print(
+        f'Results:\nAvailability A = {A_list.mean()}\nA std = {A_list.std()}\nMean queue time T_q = {queue_times_list.mean()}\nQueue time std={queue_times_list.std()}')
+
+    # Analytical approach
+    mu = 1/2
+    lam = 1/3
+    T = []
+    for i in range(4):
+        T.append(round(expected_waiting_time(lam/(mu), mu, i+1), 7))
+    fig, ax = plt.subplots()
+    ax.scatter(range(1, 5), T)
+    print(f"Wating time analytical:{T}")
+    ax.grid()
+    ax.set_xlim((0.5, 4.5))
+    ax.set_ylim((-1, 12))
+    ax.set_xlabel('Counters')
+    ax.set_ylabel('Waiting time')
+    ax.set_title("Waiting times with different amount of counters")
+    print(len(mean), len(std))
+    ax.errorbar(range(1, len(mean)+1), mean,
+                yerr=list(std), fmt="o", color="red")
+    ax.legend(["Simulation", "Analytical"])
+    plt.show()
